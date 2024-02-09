94b2804ba9999b02d77d4cf2778e432e1edf1a72
rk-rahulkamboj <>
2024-02-10 00:48:27 +0530

    Create New Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import Login from './pages/Login';
+import DashB from './pages/DashB';
+import Register from './pages/Register';
+import Projects from './pages/Projects';
 
 export default function App() {
   return (
-    <div className='text-3xl text-blue-400'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='/sign-up' element={<Register />} />
+        <Route path='/dashboard' element={<DashB />} />
+        <Route path='/sign-in' element={<Login />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
